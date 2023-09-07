df1984117494c95a41ff2a754f80f0843571586f
leonardo <leonardogtaquini@gmail.com>
2023-09-07 12:14:03 -0300

    camibos en la config de la conexion a la db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import {Sequelize, DataTypes} from 'sequelize';
 import 'dotenv/config';
 
-const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT } = process.env
+const { DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: DB_DIALECT
+const sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
+    
 });
 
 
