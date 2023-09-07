aa529af57a5d6da6d0233dd175c849c04c415ccc
leonardo <leonardogtaquini@gmail.com>
2023-09-07 11:59:37 -0300

    se corrigio el ctrl de inicio

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -14,7 +14,7 @@ const createMusicCtrl = async(req, res) => {
 }
 
 const inicioCtrl = (req, res) => {
-    return res.render('Welcome to Music');
+    return res.status(200).json({message: 'Welcome to the API'});
 }
 
 export { getAllMusicsCtrl, createMusicCtrl, inicioCtrl };
